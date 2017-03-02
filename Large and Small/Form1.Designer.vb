d2647d0f215a4580dc14ff9aa841d18a538f751a
Moolvie <moolvie@gmail.com>
2017-03-02 09:56:41 -0500

    First commit

---
+++ Large and Small/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtIntA = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtIntB = New System.Windows.Forms.TextBox()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.btnCompare = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(113, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(187, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Two Integers"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(43, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "A:"
+        '
+        'txtIntA
+        '
+        Me.txtIntA.Location = New System.Drawing.Point(72, 86)
+        Me.txtIntA.Name = "txtIntA"
+        Me.txtIntA.Size = New System.Drawing.Size(100, 20)
+        Me.txtIntA.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(208, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(24, 18)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "B:"
+        '
+        'txtIntB
+        '
+        Me.txtIntB.Location = New System.Drawing.Point(238, 86)
+        Me.txtIntB.Name = "txtIntB"
+        Me.txtIntB.Size = New System.Drawing.Size(100, 20)
+        Me.txtIntB.TabIndex = 4
+        '
+        'lblMessage
+        '
+        Me.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMessage.Location = New System.Drawing.Point(12, 139)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(390, 23)
+        Me.lblMessage.TabIndex = 5
+        Me.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCompare
+        '
+        Me.btnCompare.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCompare.Location = New System.Drawing.Point(33, 184)
+        Me.btnCompare.Name = "btnCompare"
+        Me.btnCompare.Size = New System.Drawing.Size(119, 52)
+        Me.btnCompare.TabIndex = 6
+        Me.btnCompare.Text = "&Compare"
+        Me.btnCompare.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(250, 184)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(119, 52)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(414, 261)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCompare)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.txtIntB)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtIntA)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Large and Small"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtIntA As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtIntB As System.Windows.Forms.TextBox
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
+    Friend WithEvents btnCompare As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Large and Small/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the form
+        Me.Close()
+    End Sub
+
+    Private Sub btnCompare_Click(sender As Object, e As EventArgs) Handles btnCompare.Click
+        ' Declare variable
+        Dim intNumberA As Integer
+        Dim intNumberB As Integer
+
+        If Integer.TryParse(txtIntA.Text, intNumberA) Then
+            ' Check second number
+            If Integer.TryParse(txtIntB.Text, intNumberB) Then
+                'Compare the integers
+                If intNumberA > intNumberB Then
+                    lblMessage.Text = " Value A is greatest."
+                ElseIf intNumberA < intNumberB Then
+                    lblMessage.Text = " Value B is greatest."
+                Else
+                    lblMessage.Text = " The Values are equal."
+                End If
+            Else
+                ' Display error message
+                lblMessage.Text = " you must enter an integer for B"
+            End If
+        Else
+            ' display error message for A
+            lblMessage.Text = "you must enter an integer for A"
+        End If
+    End Sub
+End Class
